a0364e3e02fdd3d1ce4227208d80a5aca077ab39
Daniel <axhammar.daniel@gmail.com>
2018-01-12 09:33:58 +0100

    Add BankATM folder with test

---
+++ src/BankATM/Account.java
@@ -0,0 +1,16 @@
+package BankATM;
+
+public class Account {
+
+
+    private double balance;
+    private double deposit;
+    private double withdraw;
+
+    public void depositAccount(){
+
+        balance += deposit;
+
+    }
+
+}
---
+++ src/Test/BankATM/AccountTest.java
@@ -0,0 +1,23 @@
+package BankATM;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void depositAccount() throws Exception {
+
+    }
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+
+}
